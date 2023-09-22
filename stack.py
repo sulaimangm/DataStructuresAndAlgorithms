3d9b893c33a63e9999931689401a12050451b9cd
sulaimangm <sulaimangm@gmail.com>
2023-09-22 23:32:54 +0300

    Adding Code for Reading Mazes

--- stack.py
+++ stack.py
@@ -16,7 +16,7 @@ class Stack:
     def pop(self):
         return self.items.pop()
     
-    def peef(self):
+    def peek(self):
         return self.items[-1]
     
     def size(self):
@@ -27,3 +27,14 @@ class Stack:
     
 if __name__ == "__main__":
     s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(3)
+    print(s)
+    s.push(7)
+    s.push(5)
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s.size())
